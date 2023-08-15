872ef0c2cc08ee44bc285120c244b8ff01cdff9b
vivekChenna <chennavivek96@gmail.com>
2023-08-15 07:08:23 +0530

    first commit created all components

---
+++ src/components/Testimonial.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Card  from "./Card";
+
+const Testimonial = () => {
+  return <div></div>;
+};
+
+export default Testimonial;
---
+++ src/constants/data.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    name: "Sundar Pichai",
+    img: "https://pbs.twimg.com/profile_images/864282616597405701/M-FEJMZ0_400x400.jpg",
+    role: "Chief Executive Officer of Google",
+  },
+  {
+    name: "Satya Nadela",
+    img: "https://pbs.twimg.com/profile_images/1221837516816306177/_Ld4un5A_400x400.jpg",
+    role: "Chief Executive Officer of MicroSoft",
+  },
+  {
+    name: "Ajay Banga",
+    img: "https://upload.wikimedia.org/wikipedia/commons/1/1f/Ajay_Banga_WBG.jpg",
+    role: "President of the world Bank",
+  },
+  {
+    name: "Arvind Krishna",
+    img: "https://www.crn.com/resources/0259-0e959bae92d1-b40f0189cdb7-1000/krishna-arvind-ibm1.jpeg",
+    role: "CEO of IBM",
+  },
+  {
+    name: "N. R. Narayana Murthy",
+    img: "https://www.infosys.com/content/dam/infosys-web/en/global-resource/photos/nrn.jpg",
+    role: "Founder of Infosys",
+  },
+];
+
+export default data;
